a6480fdaa95a805ad0d845be2afcf2fab164f2d2
LuisCaceres <luiscaceresmx@gmail.com>
2025-01-10 00:32:12 +1100

    Feature: Implement error handling
    Before, it was easy to make a mistake so that the number of headers didn't match the number of cells in each row. In that case, the list of CSV values results in a malformed table.
    
    Now, there's error handling that alerts when the number of cells don't match the number of table headers.

--- src/table-creation-with-csv-values.ts
+++ src/table-creation-with-csv-values.ts
@@ -1,28 +1,35 @@
 /* The following piece of code generates a list of comma separated values (CSV) so that they can be consumed by https://www.becsv.com/csv-table.php to create a HTML table. */
 
-const separator = '|';
 const headers = [
     'Artist',
     'Title',
-].join(separator);
+];
 
 const selector = 'h3';
+
+const separator = '|';
 const elements = Array.from(document.querySelectorAll(selector));
 
 const lines = elements.map(element => {
     const value1 = '';
-    const value2 = '';
-    const value3 = '';
-    const value4 = '';
-    const value5 = '';
+    // const value2 = '';
+    // const value3 = '';
+    // const value4 = '';
+    // const value5 = '';
     
     const values = [
-        value1,
-    ].join(separator);
+        value1
+    ];
 
-    return values;
+    const error = values.length !== headers.length;
+    
+    if (error) {
+        throw Error('The number of table headers do not match with the number of table cells.');
+    }
+
+    return values.join(separator);
 });
 
-lines.unshift(headers);
-console.log(lines.join('\n'));
+lines.unshift(headers.join(separator));
 
+console.log(lines.join('\n'));
