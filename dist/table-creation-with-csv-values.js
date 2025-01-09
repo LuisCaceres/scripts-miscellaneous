b0277f8d2f1fd90148f141606146537d711427ec
LuisCaceres <luiscaceresmx@gmail.com>
2025-01-09 23:56:00 +1100

    Feature: Create script to create a list of comma separated values.
    The following piece of code generates a list of comma separated values (CSV) so that they can be consumed by https://www.becsv.com/csv-table.php to create a HTML table.

---
+++ dist/table-creation-with-csv-values.js
@@ -0,0 +1,22 @@
+"use strict";
+/* The following piece of code generates a list of comma separated values (CSV) so that they can be consumed by https://www.becsv.com/csv-table.php to create a HTML table. */
+const separator = '|';
+const headers = [
+    'Artist',
+    'Title',
+].join(separator);
+const selector = 'h3';
+const elements = Array.from(document.querySelectorAll(selector));
+const lines = elements.map(element => {
+    const value1 = '';
+    const value2 = '';
+    const value3 = '';
+    const value4 = '';
+    const value5 = '';
+    const values = [
+        value1,
+    ].join(separator);
+    return values;
+});
+lines.unshift(headers);
+console.log(lines.join('\n'));
