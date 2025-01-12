b88c7551ca6b4c109dd9e7d424d31432a42ea255
LuisCaceres <luiscaceresmx@gmail.com>
2025-01-12 12:25:00 +1100

    Feature: Create anchor-element-from-link-in-text.ts file

---
+++ src/anchor-element-from-link-in-text.ts
@@ -0,0 +1,16 @@
+// The following piece of code converts a URL into an interactive link. For example, let's imagine that there's a paragraph and whithin that paragrapth there are URLS's. Those URL's aren't clickable, pressable or tappable. The following piece of code detects those URL's and makes them interactive links. 
+
+{
+    const selector = 'td';
+    const elements = Array.from(document.querySelectorAll(selector));
+
+    // For each element `element` in `elements`.
+    for (const element of elements) {
+        const link = document.createElement('a');
+        link.textContent = element.textContent?.trim() || '';
+        link.href = element.textContent?.trim() || '';
+
+        element.textContent = '';
+        element.append(link);
+    }
+}
