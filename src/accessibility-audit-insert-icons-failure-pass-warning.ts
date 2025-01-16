8a8bf7219493aad8c15713403f32d0b32b8a8f29
LuisCaceres <luiscaceresmx@gmail.com>
2025-01-16 16:04:10 +1100

    Create `accessibility-audit-insert-icons-failure-pass-warning`.

---
+++ src/accessibility-audit-insert-icons-failure-pass-warning.ts
@@ -0,0 +1,33 @@
+{
+    const columnNumber = 0;
+    const selector = `td:nth-of-type(${columnNumber + 1})`;
+    const cells = Array.from(document.querySelectorAll(selector)) as HTMLTableCellElement[];
+
+    // For each cell 'cell' in 'cells'.
+    for (const cell of cells) {
+        const textContent = cell.textContent?.toUpperCase().slice(0, 1);
+        
+        const icon = document.createElement('span');
+        icon.ariaHidden = 'true';
+        icon.classList.add('fa');
+
+        switch(textContent) {
+            case 'F':
+                icon.classList.add('fa-exclamation-circle');
+                break;
+            case 'P':
+                icon.classList.add('fa-check');
+                break;
+            case 'U':
+                icon.classList.add('fa-circle-question');
+                break;
+            case 'W':
+                icon.classList.add('fa-exclamation-triangle');
+                break;
+            default:
+                break;
+        }
+
+        cell.prepend('icon');
+    }
+}
