6a9b2935282348cefb33da5cadc4e863bc44ccad
LuisCaceres <luiscaceresmx@gmail.com>
2025-01-20 11:56:40 +1100

    Create `accessibility-audit-insert-images.ts`

---
+++ src/accessibility-audit-insert-images.ts
@@ -0,0 +1,27 @@
+{
+    // For example, it matches `{{image-1}}` and `{{image-10}}.
+    const regex = /{{(image-\d+)}}/;
+    const path = 'images/';
+    const extension = 'png';
+
+    const treeWalker = document.createTreeWalker(document, NodeFilter.SHOW_TEXT);
+    let currentNode;
+
+    while (currentNode = treeWalker.nextNode() as Text) {
+
+        if (currentNode) {
+            const text = (currentNode.textContent || '').trim();
+            const match = text.match(regex);
+
+            if (match) {
+                const file = match[1];
+
+                const link = document.createElement('a');
+                link.href = `${path}/${file}.${extension}`;
+                link.target = '_blank';
+
+                currentNode.replaceWith(link);
+            }
+        }
+    }
+}
