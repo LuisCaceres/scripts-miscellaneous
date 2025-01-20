632b257deede64601c810678e0ecd252004e461e
LuisCaceres <luiscaceresmx@gmail.com>
2025-01-20 12:26:08 +1100

    Update `accessibility-audit-insert-images.ts`

--- src/accessibility-audit-insert-images.ts
+++ src/accessibility-audit-insert-images.ts
@@ -6,22 +6,30 @@
 
     const treeWalker = document.createTreeWalker(document, NodeFilter.SHOW_TEXT);
     let currentNode = treeWalker.nextNode() as Text;
+    const nodes: Text[] = [];
 
     while (currentNode) {
         const text = (currentNode.textContent || '').trim();
         const match = text.match(regex);
 
         if (match) {
-            const file = match[1];
-
-            const link = document.createElement('a');
-            link.href = `${path}/${file}.${extension}`;
-            link.target = '_blank';
-            link.textContent = `${file}.${extension}`;
-
-            currentNode.replaceWith(link);
+            nodes.push(currentNode);
         }
 
         currentNode = treeWalker.nextNode() as Text;
     }
+
+
+    // For each node 'node' in 'nodes'.
+    for (const node of nodes) {
+        const file = node?.textContent?.match(regex)?.[1];
+        const link = document.createElement('a');
+        link.href = `${path}/${file}.${extension}`;
+        link.target = '_blank';
+        link.textContent = `${file}.${extension}`;
+        node.replaceWith(link);
+    }
+
+
+
 }
