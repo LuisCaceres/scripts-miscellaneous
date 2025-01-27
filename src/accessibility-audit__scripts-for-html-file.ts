e32a42150167c6fe682d57daf1a0cdd9dd400c50
LuisCaceres <luiscaceresmx@gmail.com>
2025-01-28 09:42:24 +1100

    Update

--- src/accessibility-audit__scripts-for-html-file.ts
+++ src/accessibility-audit__scripts-for-html-file.ts
@@ -1,22 +1,33 @@
 /* The HTML file that contains an accessibility audit should reference this file with a `script` tag to include the functionality below. */
 
-// Save data on server when pressing the keys `ctrl` and `s`.
-window.addEventListener('keydown', async event => {
 
-    if (event.ctrlKey && (event.key === 'S' || event.key === 's')) {
-        event.preventDefault();
+// Let `editionHistoryItems` be an initially empty list of modifications to the current document. This is relevant to undo and redo functionality.
+const editionHistoryItems: string[] = [];
 
-        const html = document.documentElement.outerHTML;
+{
 
-        const response = await fetch('foo', {
-            body: html,
-            method: 'POST',
-        });
-    }
-});
+    // Save data on server when pressing the keys `ctrl` and `s`.
+    window.addEventListener('keydown', async event => {
+    
+        if (event.ctrlKey && (event.key === 'S' || event.key === 's')) {
+            event.preventDefault();
+    
+            const html = document.documentElement.outerHTML;
+    
+            // Send data to server.
+            const response = await fetch('foo', {
+                body: html,
+                method: 'POST',
+            });
 
-// Allow edition of data.
-document.designMode = 'on';
-
-// Prevent loss of data.
-window.addEventListener('beforeunload', event => event.preventDefault());
+            // Save data as an edit history item.
+            editionHistoryItems.push(html);
+        }
+    });
+    
+    // Allow edition of data.
+    document.designMode = 'on';
+    
+    // Prevent loss of data.
+    window.addEventListener('beforeunload', event => event.preventDefault());
+}
--- src/table__do-something-with-a-column.ts
+++ src/table__do-something-with-a-column.ts
@@ -1,8 +1,15 @@
 {
     // Let `position` be the column number of the new column to be inserted.
     const position = 4;
-    const selector = `td:nth-of-type(${position})`;
+    
     const table = $0 as HTMLTableElement;
+    
+    if (table.nodeName.toUpperCase() !== 'TABLE') {
+        alert('Please select a `table` element before proceeding.');
+        throw Error(`Elemented selected isn't a \`table\` element.`);
+    }
+    
+    const selector = `td:nth-of-type(${position})`;
     const cells = Array.from(table.querySelectorAll(selector)) as HTMLTableCellElement[];
 
     // For each cell 'cell' in 'cells'.
