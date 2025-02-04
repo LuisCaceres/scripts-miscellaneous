b4af6a73ec52e745bbd62fd93efddb980e536ead
LuisCaceres <luiscaceresmx@gmail.com>
2025-02-04 15:41:51 +1100

    Update

---
+++ src/table__add-row.ts
@@ -0,0 +1,21 @@
+{
+    const rowNumber = -1;
+    let numberOfNewRows = 70;
+    
+    const table = $0 as HTMLTableElement;
+
+    if (table.nodeName.toUpperCase() !== 'TABLE') {
+        alert('Please select a `table` element before proceeding.');
+        throw Error(`Elemented selected isn't a \`table\` element.`);
+    }
+
+    const row = Array.from(table.rows).at(rowNumber) as HTMLTableRowElement;
+    const clones: HTMLTableRowElement[] = [];
+
+    while (numberOfNewRows--) {
+        const clone = row?.cloneNode(true) as HTMLTableRowElement;
+        clones.push(clone);
+    }
+
+    table.append(...clones);
+}
