1bf97391ba8a6717b36fda6154e7b8fe941c835f
LuisCaceres <luiscaceresmx@gmail.com>
2025-02-12 20:53:37 +1100

    Update

---
+++ src/table__do-something-with-a-row.ts
@@ -0,0 +1,21 @@
+{
+    // Let `table` be a HTML table.
+    // Let `startRow` be the row number of a row in `table` . 
+    // Let `endRow` be the row number of anoter row in `table`.
+    const startRow = 0;
+    const endRow = undefined;
+    
+    const table = $0 as HTMLTableElement;
+    
+    if (table.nodeName.toUpperCase() !== 'TABLE') {
+        alert('Please select a `table` element before proceeding.');
+        throw Error(`Elemented selected isn't a \`table\` element.`);
+    }
+    
+    const rows = Array.from(table.rows).slice(startRow, endRow) as HTMLTableRowElement[];
+
+    // For each row 'row' in 'rows'.
+    for (const row of rows) {
+        row;
+    }
+}
