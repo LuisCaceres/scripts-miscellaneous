e611737be2caa382a5ba63e6b8ef700581915d90
LuisCaceres <luiscaceresmx@gmail.com>
2025-03-14 16:54:28 +1100

    Update

--- src/accessibility-audit__scripts-for-html-file.ts
+++ src/accessibility-audit__scripts-for-html-file.ts
@@ -5,13 +5,15 @@ import { generateShortVersion } from "./accessibility-audit__generate-short-vers
 // Let `editionHistoryItems` be an initially empty list of modifications to the current document. This is relevant to undo and redo functionality.
 const editionHistoryItems: string[] = [];
 
+const errorMessage = 'Modifications to the file cannot currently be saved. This is because this Internet browser cannot communicate to the server. To save this file, please open the command line or terminal at the root folder of this accessibility evaluation. Then type "node server".'
+
 {
     // Verify that the server has been started so that modifications to this file can be saved.
     (async function () {
         try {
             await fetch('foo');
         } catch (error) {
-            alert('Modifications to the file cannot be saved. To do so, please open the command line or terminal at the root folder of this accessibility evaluation. Then type "node server".');
+            alert(errorMessage);
         }
     })();
 
@@ -24,11 +26,16 @@ const editionHistoryItems: string[] = [];
 
             const html = htmlElement.outerHTML;
 
-            // Save long version of evalaution on server.
-            await fetch('foo', {
-                body: html,
-                method: 'POST',
-            });
+            // Save long version of evaluation on the server.
+            try {
+                await fetch('foo', {
+                    body: html,
+                    method: 'POST',
+                });
+            } catch (error) {
+                // Alert the user if the evaluation can't be saved on the server.
+                alert(errorMessage);
+            }
 
             const shortVersion = generateShortVersion(htmlElement.cloneNode(true) as HTMLHtmlElement);
 
