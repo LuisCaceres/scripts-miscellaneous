32ec7806d380654c6e1a3e7ef82c6156a94c1ea2
Luis Caceres Castillo <luis.cacerescastillo@visionaustralia.org>
2026-06-12 12:11:12 +1000

    Update

--- src/puppeteer/projects/dark-mode/settings.ts
+++ src/puppeteer/projects/dark-mode/settings.ts
@@ -21,12 +21,19 @@ const urls = [
     `https://www.visionaustralia.org/`,
 ];
 
+//  Let `files` be a list of CSS and JS files to be loaded in a tab. The files will load in the specified order. These files will load via a `script` or `link` element. The element will be appended to the `head` element's list of children. Make sure to modify `interceptions` in this file accordingly.
+const files = [
+    'test.css',
+    'test.js',
+];
+
 // Let `urlPatterns` be a list of patterns that Pupeteer can use to check if a web page should get custom stylysheet or script insertion. Custom insertion is useful when, for example, the developer wants to test CSS or JS code on a web page.
 const urlPatterns = [
     'https://www.visionaustralia.org/',
 ];/* .map(urlPattern => new URLPattern(urlPattern)); */
 
 export {
+    files,
     interceptions,
     urlPatterns,
     urls,
--- src/puppeteer/projects/drupal-forms/settings.ts
+++ src/puppeteer/projects/drupal-forms/settings.ts
@@ -77,6 +77,12 @@ const urls = [
     // // `https://www.visionaustralia.org/form/contact`,
 ];
 
+//  Let `files` be a list of CSS and JS files to be loaded in a tab. The files will load in the specified order. These files will load via a `script` or `link` element. The element will be appended to the `head` element's list of children. Make sure to modify `interceptions` in this file accordingly.
+const files = [
+    'test.css',
+    'test.js',
+];
+
 // Let `urlPatterns` be a list of patterns that Pupeteer can use to check if a web page should get custom stylysheet or script insertion. Custom insertion is useful when, for example, the developer wants to test CSS or JS code on a web page.
 const urlPatterns = [
     // 'https://www.visionaustralia.org/*',
@@ -87,6 +93,7 @@ const urlPatterns = [
 ];/* .map(urlPattern => new URLPattern(urlPattern)); */
 
 export {
+    files,
     interceptions,
     urlPatterns,
     urls,
